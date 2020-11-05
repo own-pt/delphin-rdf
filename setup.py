070ee68d533f7eacbc883a7e18b61aaec02efce5
Fredsonerd <fredsonerd@hotmail.com>
2020-11-05 00:26:08 -0300

    update setup.py

--- setup.py
+++ setup.py
@@ -1,32 +1,41 @@
-"""
-DELPH-IN formats in RDF
-"""
+#!/usr/bin/env python3
 
+import os
 from setuptools import setup
 
+# reading METADATA from __about__.py
+about = dict()
+base = os.path.dirname(__file__)
+path = os.path.join(base, "delphin", "rdf", "__about__.py")
+exec(open(path,"r").read(), about)
+
+# reading long description from README.md
+long_description = open("README.md","r").read()
+ 
+# defining setup METADATA
 setup(
-    name="Delphin-RDF",
-    version="1.0.0",
-    # author="",
-    # author_email="",
-    # maintainer="",
-    # maintainer_email="",
-    # url="https://github.com/arademaker/delph-in-rdf",
-    # download_url="https://github.com/arademaker/delphin-rdf",
-    description=__doc__,
-    long_description=open("README.md","r").read(),
+    name=about["__name__"],
+    version=about["__version__"],
+    description=about["__summary__"],
+    long_description=long_description,
     long_description_content_type="text/markdown",
-    license="MIT",
-    # scripts=[],
+    license=about["__licence__"],
     packages=[
        "delphin",
        "delphin.rdf",
        "delphin.cli",
        "delphin.codecs"],
-    # classifiers=[],
-    # platforms="",
-    # keywords="delphin, pydelphin, rdf, mrs, eds, dm, rmrs"
+    keywords="delphin pydelphin rdf mrs eds dmrs",
     install_requires=[
        "pydelphin",
        "rdflib"],
+    # author=about["__author__"],
+    # author_email=about["__email__"],
+    # maintainer="",
+    # maintainer_email="",
+    # url=about["__url__"],
+    # download_url="",
+    # scripts=[],
+    # classifiers=[],
+    # platforms="",
 )
